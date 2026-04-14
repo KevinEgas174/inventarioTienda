40a87d276e967083df7aa0ec21c3b1e47e05652d
Kevin Egas <alexanderegas174@gmail.com>
2026-04-14 17:17:42 -0500

    Creacion del Repositorio grupal

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+    Universidad de las Américas (UDLA)
+    Carrera: Ingeniería de Software
+    Materia: Programación 1 
+    Proyecto: Gestion de inventario tiendas 
+    Autores: Kevin Alexander Egas Pozo,
+             Cesar Yunga,
+             Christian Fiallos,
+             Jardi Caseres,
+*/
+
+/*
+    HISTORIAL DE CAMBIOS:
+    2026-04-14   KEP   Creacion de Repositorio y main.c
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Escribir el codigo");
+    
+}
